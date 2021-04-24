6c0b73956b53cb9ffb1d93cdf0eae1f2acd72ffc
Jose Pedro Valentim Oliveira <s106786@ap.be>
2021-04-24 10:53:26 +0200

    temp

---
+++ src/components/FormField.vue
@@ -0,0 +1,43 @@
+<template>
+    <div name="form" class="flex flex-row  w-full justify-around  items-center bg-yellow-300 p-1 text-black rounded">
+        <div class="text-center">
+            <p>Form</p>
+            <input v-model="form" @change="populate()" class="border border-gray-500 rounded h-8  bg-gray-100 text-center w-50"  name="extraForms">
+        </div>
+        <div class="text-center">
+            <p>Regular</p>
+            <input class="border border-gray-500 rounded h-8  bg-gray-100 text-center w-96" v-model="regular"  name="extraFormsRegular">
+        </div>
+         <div class="text-center" >
+            <img class="w-16" :src="this.regular">
+        </div>      
+       
+        <div class="text-center">
+            <p>Shiny</p>
+            <input class="border border-gray-500 rounded h-8  bg-gray-100 text-center w-96" v-model="shiny"  name="extraFormsShiny">
+        </div>  
+        <div class="text-center w-16" >
+            <img  :src="this.shiny">
+        </div>             
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            regular:"",
+            shiny:"",
+            form:""
+        }
+    },
+    props:['id'],
+    methods:{
+        populate(){
+            console.log(this.id);
+            this.regular = `https://livingdexapi.herokuapp.com/sprites/regular/${this.id}-${this.form.toLowerCase()}.png`
+            this.shiny = `https://livingdexapi.herokuapp.com/sprites/shiny/${this.id}-${this.form.toLowerCase()}.png`
+        }
+    }
+}
+</script>
--- src/pages/AddPokemon.vue
+++ src/pages/AddPokemon.vue
@@ -9,7 +9,7 @@
             </div>
             <div v-show='general' class="flex flex-row  items-start w-11/12 justify-between ">
             
-                <div class="flex flex-row w-6/12  border  p-2">
+                <div class="flex flex-row w-6/12  border  p-2  h-56">
                 
                     <div class="flex flex-col w-full gap-4">
                         <div class=" w-3/12 flex flex-row justify-center items-center gap-4 ">
@@ -26,13 +26,14 @@
                             </div>
                             
                         </div>
-                        <div class=" w-5/12 flex flex-row justify-center items-center gap-4 ">
+                        <div class=" w-5/12 flex flex-row justify-center items-center space-y-4 ">
                             <div class="w-full ">
-                                <p class="float-left">Name: </p>
-                                <input v-model="name" placeholder="Fill in name" class="w-full text-black p-1 text-center border-gray-500 rounded border h-8"  type="text" id="name" name="name"><br>
+                                <p class="float-left">Form: </p>
+                                <input @change="populate()" v-model="form" placeholder="Fill in form" class="w-full text-black p-1 text-center border-gray-500 rounded border h-8" type="text" id="form" name="form"><br>
                             </div>
                             
                         </div>
+                       
                     </div>
                     <div class="flex flex-col w-5/12 ">
                         <div class="flex flex-row border border-white rounded-t h-1/12 p-1">
@@ -45,14 +46,14 @@
                         </div>
                         
                         <div class="flex flex-row border border-white h-2/12 justify-center p-1 rounded-b">
-                            <p class="">{{name}} {{species}}</p>
+                            <p class="">{{form}} {{species}}</p>
                             
                         </div>
                     </div>
                 </div>
 
 
-                <div class="w-6/12 flex flex-col items-center gap-4 p-2 border-r border-t border-b">
+                <div class="w-6/12 flex flex-col items-center gap-4 p-2 border-r border-t border-b  h-56" >
                 
 
                 <div class="w-10/12 text-center justify-center items-center  ">
@@ -171,6 +172,32 @@
                 
                 
             </div>
+            
+            <div class="w-11/12 ">
+                <div @click="forms = !forms" class="bg-red-900 rounded p-2 w-full flex flex-row items-center justify-between mb-4">
+                    <h1 class="text-2xl ">Forms :</h1>
+                    <div @click.stop @click="addForm()" class="text-white">
+                        <svg class="w-8" version="1.1" id="Icons" stroke="none" fill="currentcolor" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 32 32" style="enable-background:new 0 0 32 32;" xml:space="preserve"><path d="M25,2H7C5.3,2,4,3.3,4,5v22c0,1.7,1.3,3,3,3h14c3.9,0,7-3.1,7-7V5C28,3.3,26.7,2,25,2z M13,24h-1v1c0,0.6-0.4,1-1,1s-1-0.4-1-1v-1H9c-0.6,0-1-0.4-1-1s0.4-1,1-1h1v-1c0-0.6,0.4-1,1-1s1,0.4,1,1v1h1c0.6,0,1,0.4,1,1S13.6,24,13,24z M19.5,26c-0.8,0-1.5-0.7-1.5-1.5s0.7-1.5,1.5-1.5s1.5,0.7,1.5,1.5S20.3,26,19.5,26z M22.5,23c-0.8,0-1.5-0.7-1.5-1.5s0.7-1.5,1.5-1.5s1.5,0.7,1.5,1.5S23.3,23,22.5,23z M24,17c0,0.6-0.4,1-1,1H9c-0.6,0-1-0.4-1-1V7c0-0.6,0.4-1,1-1h14c0.6,0,1,0.4,1,1V17z"/></svg>
+                    </div> 
+                    
+                </div>
+                <div v-show="forms" class="flex flex-col gap-1">
+                    <component  v-for="component in formsArray" :is="component.component" :key="component.id" :id="id" @removeGame="removeGame(component.id)"></component>
+                </div>
+                
+                
+            </div>
+
+            <div class="w-11/12 ">
+                <div @click="abilities = !abilities" class="bg-red-900 rounded p-2 w-full flex flex-row items-center justify-between mb-4">
+                    <h1 class="text-2xl ">Abilities:</h1>
+                    <div @click.stop  class="text-white">
+                        <svg class="w-8" version="1.1" id="Icons" stroke="none" fill="currentcolor" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 32 32" style="enable-background:new 0 0 32 32;" xml:space="preserve"><path d="M25,2H7C5.3,2,4,3.3,4,5v22c0,1.7,1.3,3,3,3h14c3.9,0,7-3.1,7-7V5C28,3.3,26.7,2,25,2z M13,24h-1v1c0,0.6-0.4,1-1,1s-1-0.4-1-1v-1H9c-0.6,0-1-0.4-1-1s0.4-1,1-1h1v-1c0-0.6,0.4-1,1-1s1,0.4,1,1v1h1c0.6,0,1,0.4,1,1S13.6,24,13,24z M19.5,26c-0.8,0-1.5-0.7-1.5-1.5s0.7-1.5,1.5-1.5s1.5,0.7,1.5,1.5S20.3,26,19.5,26z M22.5,23c-0.8,0-1.5-0.7-1.5-1.5s0.7-1.5,1.5-1.5s1.5,0.7,1.5,1.5S23.3,23,22.5,23z M24,17c0,0.6-0.4,1-1,1H9c-0.6,0-1-0.4-1-1V7c0-0.6,0.4-1,1-1h14c0.6,0,1,0.4,1,1V17z"/></svg>
+                    </div> 
+                </div>
+                
+                
+            </div>
             <div class="w-11/12 ">
                 <div @click="moves = !moves" class="bg-red-900 rounded p-2 w-full flex flex-row items-center justify-between mb-4">
                     <h1 class="text-2xl ">Moves:</h1>
@@ -206,6 +233,7 @@
 <script>
 import GameMoveField from '../components/GameMoveField'
 import LocationField from '../components/LocationField'
+import FormField from '../components/FormField'
 
 export default {
     name:"addpokemon",
@@ -213,7 +241,7 @@ export default {
         return{
             id:'',
             species:'',
-            name:'',
+            form:'',
             genderless:false,
             regular:"",
             regularF:"",
@@ -228,14 +256,17 @@ export default {
             eggGroups:["Amorphous","Bug","Dragon","Fairy","Field","Flying","Grass","Human-Like","Mineral","Monster","Water 1","Water 2","Water 3","Ditto","Undiscovered"],
             gamemoves:[],
             gamelocations:[],
+            formsArray:[],
             componentId:0,
             general:true,
             sprites:true,
             moves:true,
-            locations:true
+            locations:true,
+            abilities:true,
+            forms: true
         }
     },
-    children:[GameMoveField,LocationField],
+    children:[GameMoveField,LocationField,FormField],
     methods:{
         changeFemale(maleRatio){
             this.femaleRatio = 100 - maleRatio;
@@ -269,9 +300,35 @@ export default {
             const pos = this.gamelocations.map((e)=>{return e.id}).indexOf(id);
             this.gamelocations.splice(pos,1);             
         },
+        addForm(){
+            this.formsArray.push({id:this.componentId,component:FormField});
+            this.componentId +=1;
+        },
         populate(){
-            this.regular = `https://livingdexapi.herokuapp.com/sprites/regular/${this.id}.png`
-            this.shiny = `https://livingdexapi.herokuapp.com/sprites/shiny/${this.id}.png`
+            let form = "";
+            switch (this.form) {
+                case "Alola":
+                    form = "-a";
+                    break;
+                case "Galar":
+                    form = "-g";
+                    break;
+                case "GMax":
+                    form = "-gmax";
+                    break;
+                default:
+                    break;
+            }
+            
+            if(this.id !== ""){
+                this.regular = `https://livingdexapi.herokuapp.com/sprites/regular/${this.id}${form}.png`
+                this.shiny = `https://livingdexapi.herokuapp.com/sprites/shiny/${this.id}${form}.png`
+            }
+            else{
+                this.regular = ``
+                this.shiny = ``
+            }
+            
         }
     }
 }
